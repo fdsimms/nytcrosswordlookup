6526fb4ec7fe0b6aac09cd666a5ad606f7576530
Frankie <fdsimms@gmail.com>
2018-05-10 02:10:12 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Timestack
+
+[Download on the Chrome Web Store][chrome-page]
+
+[![image](assets/timestack-image.png)][chrome-page]
+[chrome-page]: https://chrome.google.com/webstore/detail/timestack/hgkefoelcaofbcpfeimjmgofjdlgehof
+
+## Summary
+  Timestack allows users to set multiple timers with descriptions in order to plan out their days. It can be used in a variety of circumstances. I could imagine it working well for planning out workout routines (if you want to work out with your laptop nearby!) or planning out studying sessions.
+
+## Usage
+
+Fill in the amount of time you want a timer to run and an optional description of what the timer is for then hit the 'Add Timer' button. Do this for as many timers as you want, then press the 'Start' button to start the first timer. When the first timer is over, the second timer will automatically start.
+
+## Dependencies
+
+* [Angular.js][angular-link]
+* [Font Awesome][fa-link]
+* [Google Fonts][gf-link]
+[angular-link]: https://angularjs.org
+[fa-link]: https://fortawesome.github.io/Font-Awesome/
+[gf-link]: https://google.com/fonts
+
+## Other
+
+* Bell sound provided by Mike Koenig at [this page][sound-link].
+
+[sound-link]: http://soundbible.com/1746-Ship-Bell.html
---
+++ contentScript.js
@@ -0,0 +1,141 @@
+const buildSearchEndpoint = searchString => (
+    `https://en.wikipedia.org/w/api.php?action=opensearch&search=${searchString}&format=json&origin=*`
+);
+
+const fetchWikipediaSearchResultSummaries = async text => {
+    const search = await fetch(buildSearchEndpoint(text.toLowerCase()), {
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+            "Origin": "*"
+        }
+    });
+    const searchJson = await search.json();
+    const summaries = searchJson && searchJson[2];
+    const firstRealSummary = summaries && summaries.find(summary => (
+        summary && !summary.includes("may refer to:") && !summary.includes("several meanings:")
+    )) 
+    return firstRealSummary;
+}
+const fetchWiktionarySearchResult = async text => {
+    const search = await fetch(buildSearchEndpoint(text), {
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+            "Origin": "*"
+        }
+    });
+    const searchJson = await search.json();
+    console.log(searchJson[1][1]);
+    return searchJson[1][1];
+}
+
+window.onload = () => {
+    const resetButton = document.querySelector(".layout > div > div > ul > div button");
+    // this button only appears when puzzle has been completed i think?
+    const isPuzzleDone = resetButton && resetButton.innerHTML === "reset";
+    if (!isPuzzleDone) { return; }
+    const cells = document.querySelectorAll('g[data-group=cells] g');
+    const rows = [];
+    const columns = [];
+    // build row + column matrices
+    for (i = 0; i < cells.length; i++) {
+        const cell = cells[i];
+        const positionElement = cell.querySelector('rect');
+        const yValue = positionElement.getAttribute("y");
+        const yIndex = (parseInt(yValue) - 3) / 33;
+        const xValue = positionElement.getAttribute("x");
+        const xIndex = (parseInt(xValue) - 3) / 33;
+        const textElement = cell.querySelector('text[text-anchor=middle]');
+        const text = textElement ? textElement.innerHTML : null;
+        if (text) {
+            cell.addEventListener("click", async ({ target }) => {
+                // ensure that our word arrays have been populated
+                if (rowWords.length > 0 && columnWords.length > 0) {
+                    const yValue = positionElement.getAttribute("y");
+                    const yIndex = (parseInt(yValue) - 3) / 33;
+                    const xValue = positionElement.getAttribute("x");
+                    const xIndex = (parseInt(xValue) - 3) / 33;
+                    const columnWord = columnWords.find(word => (
+                        word.column === xIndex && yIndex >= word.startIdx && yIndex <= word.endIdx
+                    ))
+                    const rowWord = rowWords.find(word => (
+                        word.row === yIndex && xIndex >= word.startIdx && xIndex <= word.endIdx
+                    ))
+                    if (columnWord) {
+                        const columnWordSummaries = await fetchWikipediaSearchResultSummaries(columnWord.text)
+                        console.log(columnWord.text.toUpperCase() + ":")
+                        console.log("_____________________")
+                        console.log(columnWordSummaries);
+                        console.log("_____________________")
+                    }
+                    if (rowWord) {
+                        const rowWordSummaries = await fetchWikipediaSearchResultSummaries(rowWord.text)
+                        console.log(rowWord.text.toUpperCase() + ":")
+                        console.log("_____________________")
+                        console.log(rowWordSummaries);
+                        console.log("_____________________")
+                    }
+                }
+            });
+        }
+        if (!rows[yIndex]) {
+            rows[yIndex] = [];
+        }
+        if (!columns[xIndex]) {
+            columns[xIndex] = [];
+        }
+
+        rows[yIndex].push(text);
+        columns[xIndex].push(text);
+    }
+
+    const rowWords = [];
+    let currentRowWord;
+    for (i = 0; i < rows.length; i++) {
+        for (j = 0; j < rows[i].length; j++) {
+            const char = rows[i][j];
+            if (!char || j === rows[i].length - 1) {
+                if (currentRowWord) {
+                    currentRowWord.endIdx = char ? j : j - 1;
+                    currentRowWord.text = char ? `${currentRowWord.text}${char}` : currentRowWord.text;
+                    rowWords.push(currentRowWord);
+                    currentRowWord = null;  
+                }              
+            } else if (char && !currentRowWord) {
+                currentRowWord = {
+                    startIdx: j,
+                    endIdx: null,
+                    text: char,
+                    row: i
+                }
+            } else {
+                currentRowWord.text = `${currentRowWord.text}${char}`;
+            }
+        }
+    }
+
+
+    const columnWords = [];
+    let currentColumnWord;
+    for (i = 0; i < columns.length; i++) {
+        for (j = 0; j < columns[i].length; j++) {
+            const char = columns[i][j];
+            if (!char || j === columns[i].length - 1) {
+                if (currentColumnWord) {
+                    currentColumnWord.endIdx = j - 1;
+                    currentColumnWord.text = char ? `${currentColumnWord.text}${char}` : currentColumnWord.text;
+                    columnWords.push(currentColumnWord);
+                    currentColumnWord = null;  
+                }              
+            } else if (char && !currentColumnWord) {
+                currentColumnWord = {
+                    startIdx: j,
+                    endIdx: null,
+                    text: char,
+                    column: i
+                }
+            } else {
+                currentColumnWord.text = `${currentColumnWord.text}${char}`;
+            }
+        }
+    }
+}
